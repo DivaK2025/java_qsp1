854601b537c53359f16d8a7597951e6db9d35c36
DivaK2025 <khadedivyarani99@gmail.com>
2025-02-14 12:00:38 +0530

    Hii

---
+++ Pattern1.java
@@ -0,0 +1,23 @@
+ class Pattern1 
+	 {
+   
+    public static void main(String [] args)
+    {
+		int n = 6;
+        int i, j;
+
+        for (i = 1; i <= n; i++)
+
+			{
+            for (j = 1; j <= i; j++)
+			{
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+ }
+
+   
+
