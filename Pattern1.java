a3c6ca879d332c9f385dc9aef9e0828a38fb4e50
DivaK2025 <khadedivyarani99@gmail.com>
2025-02-14 12:49:04 +0530

    hello

--- Pattern1.java
+++ Pattern1.java
@@ -6,10 +6,10 @@
 		int n = 6;
         int i, j;
 
-        for (i = 1; i <= n; i++)
+        for (i = 0; i < n; i++)
 
 			{
-            for (j = 1; j <= i; j++)
+            for (j = 0; j <= i; j++)
 			{
                 System.out.print("*");
             }
