425477b4b26b0e6cb77350db18af113815965101
DivaK2025 <khadedivyarani99@gmail.com>
2025-02-19 20:48:48 +0530

    Hello

--- JavaClass/Study.java
+++
@@ -1,8 +0,0 @@
-class Study
-{
-public static void main(String args[])
-{
-System.out.println("Name:Divyarani Vasnt Khade");
-System.out.println("Degree:B.tech");
-}
-}
--- JavaClass/java_qsp1 - Copy/operators.java
+++
@@ -1,9 +0,0 @@
-class operators 
-{
-	public static void main(String[] args) 
-	{
-		Str
-		 
-	}
-}
-
