ec185e3ba16330581f901238b47e4690d3debdf9
DivaK2025 <khadedivyarani99@gmail.com>
2025-02-20 09:57:08 +0530

    hiii

---
+++ Diff.java
@@ -0,0 +1,25 @@
+class Diff
+{
+	public static void main(String[] args) 
+	{
+		int dif1 = 7; //odd
+		int dif2 = -2; // odd
+
+		for(int i =1;i<=i;i++){
+			int n =i;
+		for(int j=1;j<=i;j++){
+			System.out.print(n+ " ");
+
+			if(j%2!=0){
+				n +=dif1;
+			}else{
+				n+=dif2;
+			}
+			dif1 -= 2;
+            dif2 += 2;
+			System.out.println();
+		}
+				
+	}
+}
+}
