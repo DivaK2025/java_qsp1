9a63044888acc0decde86f781af777fc97eb75f0
DivaK2025 <khadedivyarani99@gmail.com>
2025-02-21 12:45:14 +0530

    OTP generation

--- FacinatingNumber.java
+++ FacinatingNumber.java
@@ -1,34 +1,29 @@
-class FacinatingNumber 
-{
-	public static void main(String[] args) 
-	{
-		int num =327;
-		String str = num+""+(num*2)+(num*3); // we can multiply facinating number only 2 oe 3
-		boolean flag = true;
-		//loop which starts from 1 to 9 characters
-		for(char j='1';i<='9';i++)
-		{
-			int cnt = 0; // inti char count is equalsto zero for every new char.
-			//fetch char from string one by one from index 0.
-			for(int j=0;j<ste.length();j++)
-			{
-				char ch = =str.charAt(j); // char fetched is stored in var ch
-				if(ch==i) // if i char matches with ch char if executes
-				cnt++; //increases the count if cnd satisfy
-			}
-			if(cnt!=1) // if cnt of a char is not equals to 1 then if executes{
-			{
-				flag = false;  // int i the flag is cnt is not equals to 1
-				break;  // terminating the loop
-			}
-		}
-		if(flag){
-			System.ou.println(num+ " is facinatingNumber.");
-		}
-		else
-		{
-			System.ou.println(num+ " is  not facinatingNumber.");
-		}
+class FascinatingNumber {
+    public static void main(String[] args) {
+        int num = 327;
+        String str = num + "" + (num * 2) + (num * 3); // Concatenate num, num*2, and num*3
+        boolean flag = true;
+        
+        // Check that all digits from '1' to '9' appear exactly once in the concatenated string
+        for (char i = '1'; i <= '9'; i++) {
+            int cnt = 0; // Initialize the count for the current digit
+            // Iterate through the concatenated string
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j); // Get each character from the string
+                if (ch == i) {
+                    cnt++; // Increment count if the character matches
+                }
+            }
+            if (cnt != 1) { // If any digit is missing or repeated
+                flag = false;
+                break; // Exit the loop if condition is not satisfied
+            }
+        }
 
-	}
+        if (flag) {
+            System.out.println(num + " is a Fascinating Number.");
+        } else {
+            System.out.println(num + " is not a Fascinating Number.");
+        }
+    }
 }
